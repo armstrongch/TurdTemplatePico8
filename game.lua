aa92e23728da79d06af9dff16a6fe015a77dfa4f
Turd Boomerang <christopherernestarmstrong@gmail.com>
2023-03-30 08:28:18 -0400

    starting to build function and file structure

--- game.lua
+++ game.lua
@@ -1,1 +1,13 @@
 --cd c:/users/chris/appdata/roaming/pico-8/carts/turd_template
+
+function _init()
+	state = "menu"
+end
+
+function _update()
+	if state == "menu" then update_menu() end
+end
+
+function _draw()
+	if state == "menu" then draw_menu() end
+end
---
+++ menu.lua
@@ -0,0 +1,7 @@
+function update_menu()
+
+end
+
+function draw_menu()
+
+end
